ebf88e48e715c82c224555ce33ce90c1564de6f9
JulianEgbert <Julian.Egbert@student.hpi.de>
2023-08-05 11:12:34 +0200

    add draft for new frontend

--- www/js/map.js
+++ www/js/map.js
@@ -1,55 +1,52 @@
-var pos = [52.39382298426837, 13.131691252274432];
-var map = L.map('map').setView(pos, 13);
-L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    minZoom: 10,
-    maxZoom: 19,
-    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+// Setup Map:
+var map = L.map("map", { zoomControl: false }).setView([51.163361, 10.447683], 7);
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  attribution: '',
+  maxZoom: 18
 }).addTo(map);
 
-var marker = L.marker(pos).addTo(map);
-map.setZoom(19);
+let marker, accuracyCircle, lat, long, accuracy, circle, featureGroup;
+/**
+ * Update the map according to the position object.
+ */
+function displayPosition(event) {
+  lat = event.coords.latitude;
+  long = event.coords.longitude;
+  accuracy = event.coords.accuracy;
+  heading = event.coords.heading;
 
-// create an AudioListener and add it to the camera
-let camera = new THREE.PerspectiveCamera(50, window.innerWidth / window.innerHeight, 1, 10000);
-camera.position.set(0, 25, 0);
-const listener = new THREE.AudioListener();
-camera.add(listener);
+  if (marker) {
+    map.removeLayer(marker);
+  }
 
-// create the PositionalAudio object (passing in the listener)
-const sound = new THREE.PositionalAudio(listener);
+  if (accuracyCircle) {
+    map.removeLayer(accuracyCircle);
+  }
 
-// load a sound and set it as the PositionalAudio object's buffer
-const audioLoader = new THREE.AudioLoader();
-audioLoader.load('sounds/demo.ogg', function (buffer) {
-    sound.setBuffer(buffer);
-    sound.setRefDistance(20);
-    sound.play();
+  marker = L.marker([lat, long]);
+  accuracyCircle = L.circle([lat, long], { radius: accuracy });
+  featureGroup = L.featureGroup([marker, accuracyCircle]).addTo(map);
+  map.fitBounds(featureGroup.getBounds());
+}
+
+function error(message) {
+  console.error(message);
+  document.getElementById("error").innerHTML = message;
+}
+
+// Start geolocation watcher
+if (!navigator.geolocation) {
+  error("Your browser doesn't support geolocation feature!");
+} else {
+  navigator.geolocation.watchPosition(
+    displayPosition,
+    (e) => {error(e.message);},
+    {maximumAge: 5000, timeout: 5000, enableHighAccuracy: true}
+  );
+}
+
+addEventListener("resize", (event) => {
+    if(!featureGroup) return;
+    map.fitBounds(featureGroup.getBounds());
 });
-
-// create an object for the sound to play from
-const sphere = new THREE.SphereGeometry(20, 32, 16);
-const material = new THREE.MeshPhongMaterial({ color: 0xff2200 });
-const mesh = new THREE.Mesh(sphere, material);
-scene.add(mesh);
-
-// finally add the sound to the mesh
-mesh.add(sound);
-
-function onSuccess(position) {
-    console.log("pos changed");
-    var element = document.getElementById('geolocation');
-    pos = [position.coords.latitude, position.coords.longitude];
-    console.log('Latitude:', position.coords.latitude, 'Longitude:', position.coords.longitude);
-    marker.setLatLng(pos);
-    map.setView(pos);
-}
-
-function onError(error) {
-    console.log('code:', error.code, 'message:', error.message);
-}
-
-function changedPos() {
-    var watchID = navigator.geolocation.watchPosition(onSuccess, onError, { timeout: 30000 });
-}
-
-document.addEventListener('deviceready', changedPos, false);
